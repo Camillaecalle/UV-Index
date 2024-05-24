010398a8d8cdd1071e841c9c88d65775dd506d04
Camillaecalle <camilla7732528@gmail.com>
2024-05-23 20:18:03 -0400

    f

--- public/script.js
+++ public/script.js
@@ -5,19 +5,22 @@ document.getElementById('locationForm').addEventListener('submit', function (eve
     const longitude = document.getElementById('longitude').value;
     const apiKey = 'openuv-5a4urlwjo6ckx-io';
   
-    const url = `/api/uv?lat=${latitude}&lng=${longitude}`;
+    const url = `https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`;
   
-    axios.get(url)
-      .then(response => {
+    axios.get(url, {
+        headers: {
+            'x-access-token': apiKey
+        }
+    })
+    .then(response => {
         const data = response.data;
-        const uvIndex = data.uv;
+        const uvIndex = data.result.uv;
         document.getElementById('uvIndex').textContent = uvIndex;
         document.getElementById('sunscreenMessage').textContent = uvIndex > 3 ? 'It is recommended to wear sunscreen.' : 'No sunscreen needed.';
         document.getElementById('uv-info').classList.add('visible');
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error('Error:', error);
         alert('Failed to retrieve UV index. Please try again later.');
-      });
+    });
   });
-  
